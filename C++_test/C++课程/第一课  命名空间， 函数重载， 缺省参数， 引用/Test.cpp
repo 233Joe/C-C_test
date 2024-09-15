8ad7d2cc0da861a2c28919f8731aece54febab99
233Joe <167508574+233Joe@users.noreply.github.com>
2024-09-15 15:19:25 +0800

    Add files via upload

---
+++ C++_test/C++课程/第一课  命名空间， 函数重载， 缺省参数， 引用/Test.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;//	C++库所有东西都放在std命名空间中。
+
+//命名空间的使用
+//using std::cout;
+//using std::endl;
+
+//std::cin >> i ;
+//std::cout << "qq" << i << std::endl;
+
+
+
+//缺省参数 全缺省
+void Func1(int a = 0, int b = 0, int c = 0)
+{
+	cout << "全缺省" << endl;
+}
+//半缺省 必须从右往左依次缺省
+void Func2(int a, int b, int c = 0)
+{
+	cout << "半缺省" << endl;
+}
+
+//函数重载
+//个数不同 / 顺序不同 / 类型不同
+void Add(int a, int b, int c);
+
+void Add(long a, long b, long c);
+
+void Add(long a, char b);
+
+void Add(char a, long b);
+
+
+int main()
+{
+	int i;
+	std::cin >> i ;
+	std::cout << "qq" << i << std::endl;
+	
+	
+	Func1();
+	Func2(1, 2);
+	
+	
+//引用：就是给一个变量取别名
+	int& a = i;	//引用必须在定义的时候初始化
+	a = 0;
+	cout << i << endl;
+	
+	int c = 1;
+	int& d = c;
+	const int& e = c;//权限的缩小是可以的
+	
+	
+	int b = 0;
+	double db = b;//隐士类型的转换
+	const double& rd = b;//必须要加const,因为权限缩小的问题
+	
+	return 0;
+}
+
+
---
+++ C++_test/C++课程/第四课   运算符重载实现日期运算/Test.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+using namespace std;
+
+class Date
+{
+public:
+
+	int GetMonthDay(int year, int month)
+	{
+		static int monthDays[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		if(month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+		{
+			return 29;
+		}
+		return monthDays[month];
+	}
+	
+	Date(int year = 0, int month = 1, int day = 1)
+	{
+		if(year >= 0 && (month >= 1 && month <= 12) &&
+		  (day >= 1 && day <= GetMonthDay(year, month)))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+		{
+			cout << "日期非法" << endl;
+		}
+	}
+	
+	void Print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+	
+	
+	 bool operator<(const Date& b)
+	{
+		if(_year < b._year ||
+		  (_year == b._year) && ((_month < b._month) ||
+		  (_year == b._year && _month == b._month && _day < b._day)))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	Date& operator=(const Date& b)
+	{
+		if(this != &b)
+		{
+			_year = b._year;
+			_month = b._month;
+			_day = b._day;
+		}
+		return *this;
+	}
+	
+	bool operator==(const Date& b)
+	{
+		return (_year == b._year) && (_month == b._month) && (_day == b._day);
+	}
+	
+	bool operator>(const Date& b)
+	{
+		return !(*this < b) && !(*this == b);
+	}
+	
+	bool operator<=(const Date& b)
+	{
+		return !(*this > b);
+	}
+	
+	bool operator>=(const Date& b)
+	{
+		return !(*this < b);
+	}
+	
+	bool operator!=(const Date& b)
+	{
+		return !(*this == b);
+	}
+	
+	
+	
+	Date operator+(int day)
+	{
+		Date ret(*this);
+		ret._day += day;
+		while(ret._day > GetMonthDay(ret._year, ret._month))
+		{
+			ret._day -= GetMonthDay(ret._year, ret._month);
+			ret._month++;
+			if(ret._month == 13)
+			{
+				ret._year++;
+				ret._month = 1;
+			}
+		}
+		return ret;
+	}
+	
+	Date operator-(int day)
+	{
+		Date ret(*this);
+		ret._day -= day;
+		while(ret._day < 1)
+		{
+			ret._month--;
+			if(ret._month == 0)
+			{
+				ret._year--;
+				ret._month = 12;
+			}
+			ret._day += GetMonthDay(ret._year, ret._month);// + ret._day;
+		}
+		return ret;
+	}
+	
+	Date& operator+=(int day)
+	{
+		_day += day;
+		while(_day > GetMonthDay(_year, _month))
+		{
+			_day -= GetMonthDay(_year, _month);
+			_month++;
+			if(_month == 13)
+			{
+				_year++;
+				_month = 1;
+			}
+		}
+		return *this;
+	}
+	
+	Date& operator-=(int day)
+	{
+		_day -= day;
+		while(_day < 1)
+		{
+			_month--;
+			if(_month == 0)
+			{
+				_year--;
+				_month = 12;
+			}
+			_day += GetMonthDay(_year, _month) + _day;
+		}
+		return *this;
+	}
+	
+	Date& operator--()
+	{
+		return *this -= 1;
+	}
+	
+	Date operator--(int)
+	{
+		Date tmp(*this);
+		*this -= 1;
+		return tmp;
+	}
+	
+	Date& operator++()
+	{
+		return *this += 1;
+	}
+	
+	Date operator++(int)
+	{
+		Date tmp(*this);
+		*this += 1;
+		return tmp;
+	}
+	
+	int operator-(const Date& b)
+	{
+		int day = 0;
+		while(!(*this == b))
+		{
+			if(*this > b)
+				*this-=1;
+				
+			else if(*this < b)
+				*this+=1;
+			
+			day++;
+		}
+
+		return day;
+	}
+	
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+
+int main()
+{
+	Date a(2024, 7, 1);
+	Date b(2024, 7, 25);
+//	cout << "a < b :" << (a < b) << endl;
+//	
+//	cout <<  "a > b :" << (a > b) << endl;
+//	
+//	Date d;
+//	d = a = b;
+//	d.Print();
+//	
+//	cout <<  "a == b :" << (a == b) << endl;
+//
+//	cout <<  "a <= b :" << (a <= b) << endl;
+//
+//	cout <<  "a >= b :" << (a >= b) << endl;
+//
+//	cout <<  "a != b :" << (a != b) << endl;
+//	
+//	a.Print();
+//	b.Print();
+//	
+	Date c = a + 10;
+//	c.Print();
+	
+//	c = a - 365;
+//	c.Print();
+//	
+//	a += 20;
+//	a.Print();
+//	
+//	a -= 20;
+//	a.Print();
+//	
+//	a--;
+//	a.Print();
+//	
+//	a++;
+//	a.Print();
+
+
+	cout << a - b << endl;
+	
+
+//	cout <<  "a += 10 :" << (a += 10) << endl;
+//	
+//	cout <<  "a -= 10 :" << (a -= 10) << endl;
+//	
+//	cout <<  "a - b :" << (a - b) << endl;
+
+	return 0;
+}
